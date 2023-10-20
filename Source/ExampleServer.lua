f03573f76ccd581cd56df123044b1b958711061a
threadous <108011870+threadous@users.noreply.github.com>
2023-10-20 19:08:40 -0400

    Create ExampleServer.lua

---
+++ Source/ExampleServer.lua
@@ -0,0 +1,4 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RbxEnv = require(ReplicatedStorage.RbxEnv.Env)
+
+RbxEnv.LoadClient() -- Add this code only to the client after requiring the module
